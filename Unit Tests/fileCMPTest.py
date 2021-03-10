88ac3934a9dc8aa252c667f81f966d2488cfe77f
Javier-Lomelin <70865946+Javier-Lomelin@users.noreply.github.com>
2021-03-10 10:59:20 -0600

    Update fileCMPTest.py

--- Unit Tests/fileCMPTest.py
+++ Unit Tests/fileCMPTest.py
@@ -7,12 +7,12 @@ class FileCMPTest(unittest.TestCase):
         file1 = "Text Files/file1.txt"
         file2 = "Text Files/file2.txt"
 
-        comp = filecmp.cmp(file1, file2)
-        self.assertFalse(comp)
+        a = filecmp.cmp(file1, file2)
+        self.assertFalse(a)
 
     def test_content_equal_shallow(self):
         file1 = "Text Files/file1.txt"
         file2 = "Text Files/file1_copy.txt"
 
-        comp = filecmp.cmp(file1, file2)
-        self.assertTrue(comp)
+        a = filecmp.cmp(file1, file2)
+        self.assertTrue(a)
